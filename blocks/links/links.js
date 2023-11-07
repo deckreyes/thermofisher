ff73c687ee84a88b40a92f47c1cf2dab4d90a168
deck-adobe <deckreyes@adobe.com>
2023-11-07 14:07:39 -0800

    Change to link.js

--- blocks/links/links.js
+++ blocks/links/links.js
@@ -7,7 +7,7 @@ fetch("links.json")
     //document.getElementById('list-of-links').textContent = data.data[0].Title;
 
     var mainContainer = document.getElementById("list-of-links");
-    for (var i = 0; i < data.data.length; i++) {
+    for (var i = 1; i < data.data.length; i++) {
         var div = document.createElement("div");
         div.innerHTML = "<a href=" + data.data[i].Link + ">" + data.data[i].Title + "</a>";
         mainContainer.appendChild(div);
